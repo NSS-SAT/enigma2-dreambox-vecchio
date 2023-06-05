c435ddb4f51280461130e075965332cc88206880
NEDVED <81510045+NSS-SAT@users.noreply.github.com>
2023-06-05 18:07:42 +0200

    Update About.py

--- lib/python/Screens/About.py
+++ lib/python/Screens/About.py
@@ -34,7 +34,7 @@ class About(Screen):
 		cpu = about.getCPUInfoString()
 		AboutText += _("CPU: ") + cpu + "\n"
 		AboutText += _("Image: ") + about.getImageTypeString() + "\n"
-		AboutText += _("Info: www.nonsolosat.net") + "\n"
+		AboutText += _("Forum: https://www.nonsolosat.net") + "\n"
         
 		# [WanWizard] Removed until we find a reliable way to determine the installation date
 		# AboutText += _("Installed: ") + about.getFlashDateString() + "\n"
